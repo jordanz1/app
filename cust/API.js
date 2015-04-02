3ccc26bfe82135d4d196676327e248913aab1252
Tyler Waite, Laptop <tylerwaite15@gmail.com>
2015-04-02 04:41:29 -0400

    yes

--- cust/API.js
+++ cust/API.js
@@ -156,7 +156,7 @@ function kindaSQL_updateSignup(amount){
                                         
                     paramsForPointUpdate.AttributeUpdates['pointAmount'] = {Action: 'ADD', Value: {'N': '1'}};
                     
-                
+                    console.log(typeof pointAmount);
                     if(pointAmount === 10){
                         point += 1;
                         
