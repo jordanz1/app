d45a848e5f0c73a7e7b8826d771d80e18eb7c3d2
Tyler Waite, Laptop <tylerwaite15@gmail.com>
2015-04-02 14:03:05 -0400

    test

--- cust/API.js
+++ cust/API.js
@@ -48,7 +48,7 @@ function homepageAPI(){
                     numbOfSent += 1;
                     submitSignup(userObj);
                     log("COUNT:" + numbOfSent);
-                }, 9000);
+                }, 900);
                 //s.emit('signUpReceived', true);
 
             });  
